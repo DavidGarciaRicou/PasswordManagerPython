cbda51fdc7fa800ee74cae0f153a373d3d8beb04
Sc0rp10n <71003405+DavidGarciaRicou@users.noreply.github.com>
2020-09-09 20:21:10 +0200

    PassManV0.1.0.1
    Add coments to make the code easier to understand

--- PassMan.py
+++ PassMan.py
@@ -9,23 +9,39 @@ n=1
 newline=[]
 datatoread=[]
 check=False
+
+#Check if the master password and username are correct
 while userlog!=username or userpwd!=password:
     userlog=input("Username: ")
     userpwd=gp.getpass("Password: ")
+    
+#If the login is correct proceed with the options
 if userpwd==password and userlog==username:
     while True:
-        while n!="1" and n!="2" and n!="3":
+        
+        #Select the mode 1=inputData 2=outputData 3=exit
+        while n!="1" and n!="2" and n!="3": 
             n=str(input("Do you want to introduce a new username & password (1), or see the current (2) and if you want to leave (3): "))
+            
+        #To exit the program
         if n=="3":
             sys.exit("Thanks for using this program :)")
-        if n=="1": 
+            
+        #To input the data
+        if n=="1":  
             fo=open(reqfile,'r')
             data=csv.reader(fo)
             datatoread=list(data)
             while check==False:
+                
+                #Input the platform name
                 newline.append(input("Introduce platform, if you want to go back input (0): "))
+                
+                #Check if the user wants to go back
                 if newline[0]=="0":
                     check=True
+                    
+                #Checks if the platform was already created
                 if newline[0]!="0":
                     for x in range(0,len(datatoread)):
                         platcheck=datatoread[x]
@@ -40,6 +56,8 @@ if userpwd==password and userlog==username:
                             check=True
                     except:
                         pass
+                    
+            #Fills the username and the password for the specified platform after checking if the user wants to go back
             if newline[0]!="0":
                 fo.close
                 newline.append(input("Introduce username: "))
@@ -53,14 +71,17 @@ if userpwd==password and userlog==username:
             n="0"
             check=False
             newline=[]
-            
-        if n=="2":
+        
+        #To output the data
+        if n=="2": 
             while check==False:
                 pltoread=input("Pleas introduce the name of the platform you want to see username and password, or (all) to show them all: ")
                 fo=open(reqfile,'r',newline="") #r to read; newline="" to prevent empty lines
                 data=csv.reader(fo)
                 datatoread=list(data)
-                if pltoread=="all":
+                
+                #Output all data
+                if pltoread=="all": 
                     for x in range(1,len(datatoread)):
                         platcheck=datatoread[x]
                         print("Platform: ",platcheck[0])
@@ -68,7 +89,9 @@ if userpwd==password and userlog==username:
                         print("Password: ",platcheck[2])
                         print("=======================")
                         check=True
-                elif pltoread!="all":
+                        
+                #Output only selected platform
+                elif pltoread!="all": 
                     for x in range(len(datatoread)):
                         platcheck=datatoread[x]
                         plattochk=platcheck[0]
